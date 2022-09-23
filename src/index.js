e671d9590a4b343e4ce1e753006a7e5e3d3559a5
Vanessa Mercado <finessevanes@Vanessas-MacBook-Pro.local>
2022-09-22 19:11:53 -0700

    install wc and clean up index

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { Web3ModalProvider } from "@web3modal/react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const config = {
+  projectId: "<YOUR_PROJECT_ID>",
+  theme: "dark",
+  accentColor: "default",
+  ethereum: {
+    appName: "web3Modal",
+  },
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Web3ModalProvider config={config}>
     <App />
-  </React.StrictMode>
+  </Web3ModalProvider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
